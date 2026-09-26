b84ab3f9d2309406bcf0f874d4d689d002aae1db
wu716 <wulalaminka@gmail.com>
2026-09-26 15:33:59 +0800

    Restore schedule scrolling and mode visibility

--- static/js/daily-schedule.js
+++ static/js/daily-schedule.js
@@ -236,7 +236,7 @@
         }
         if (addRow) addRow.hidden = mode !== 'list';
         const later = document.getElementById('scheduleLater');
-        if (later) later.hidden = false;
+        if (later) later.hidden = mode !== 'list';
         updateAddComposer();
     }
 
